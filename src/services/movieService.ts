6ef9a4593c49dcbad1fa77c26afa0c828ee2336e
Artem <artemkolesnik0201@gmail.com>
2025-06-13 18:12:59 +0100

    1

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,21 +1,26 @@
 import axios from "axios";
-import { type Movie } from "../types/movie";
+import type { Movie } from "../types/movie";
 
-interface Movies {
+interface FetchResponse {
   results: Movie[];
   total_pages: number;
 }
 
-export default async function fetchMovies(query: string, currentPage: number) {
-  const url = `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=${currentPage}`;
-
-  const options = {
-    headers: {
-      Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MjIxNjNhMGJmODcwNTM0ZjdjMWRlMzg0ZDlmZDYxZCIsIm5iZiI6MTc0OTQ5MDY2Mi4xMjk5OTk5LCJzdWIiOiI2ODQ3MWJlNjdmM2RlZjZmODEzMDFiZTQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.Pr0Cq4JlvRJfVOvsYJo6-CafmnjINgvR3m7t7A9w2-o`,
-    },
-  };
-
-  const res = await axios.get<Movies>(url, options);
+export default async function getMovies(
+  topic: string,
+  page: number
+): Promise<FetchResponse> {
+  const res = await axios.get<FetchResponse>(
+    `https://api.themoviedb.org/3/search/movie?query=${topic}`,
+    {
+      headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_API_TOKEN}`,
+      },
+      params: {
+        page,
+      },
+    }
+  );
 
   return res.data;
 }
